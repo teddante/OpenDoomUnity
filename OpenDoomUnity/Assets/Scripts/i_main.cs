d394082d9141177a9e19dbfd56e16b150145bf68
Ted Enston <edwardenston@gmail.com>
2022-11-05 13:21:37 +0000

    i_main

---
+++ OpenDoomUnity/Assets/Scripts/i_main.cs
@@ -0,0 +1,13 @@
+﻿public static class Globals
+{
+	internal const string rcsid = "$Id: i_main.c,v 1.4 1997/02/03 22:45:10 b1 Exp $";
+	
+	internal static void Main(int argc, string[] args)
+	{
+		myargc = argc;
+		myargv = args;
+
+		D_DoomMain();
+
+	}
+}
