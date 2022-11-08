ccfbadc9e3b93efffbbd0bee458b8fc1b3ed7c84
Ted Enston <edwardenston@gmail.com>
2022-11-08 16:05:31 +0000

    Update i_main.cs

--- OpenDoomUnity/Assets/Scripts/i_main.cs
+++ OpenDoomUnity/Assets/Scripts/i_main.cs
@@ -1,7 +1,5 @@
 ﻿public static partial class Globals
 {
-	internal const string rcsid = "$Id: i_main.c,v 1.4 1997/02/03 22:45:10 b1 Exp $";
-	
 	internal static void Main(int argc, string[] args)
 	{
 		myargc = argc;
